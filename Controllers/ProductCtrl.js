ed6b9d0c185d23a358a6346ce737f9baefb5d24a
siyamrh7 <siyamrh7@gmail.com>
2021-07-21 18:13:24 +0600

    bugs

--- Controllers/ProductCtrl.js
+++ Controllers/ProductCtrl.js
@@ -3,13 +3,13 @@ const createProduct = async (req, res) => {
   try {
     const { name, images, price } = req.body;
     if (!name || !price || !images) {
-      return res.json({msg:"Please enter required filled"});
+      return res.json({ msg: "Please enter required filled" });
     }
     const product = new Products({
       ...req.body,
     });
     const prod = await product.save();
-    res.json({msg:`Product ${prod.name} Created`});
+    res.json({ msg: `Product ${prod.name} Created` });
   } catch (error) {
     res.send(error.message);
   }
@@ -17,10 +17,9 @@ const createProduct = async (req, res) => {
 
 const getProducts = async (req, res) => {
   try {
-    const { price, createdAt, currency, page, search } = req.query;
-    const limit = 12;
+    const { limit,price, createdAt, currency, page, search ,} = req.query;
     const skip = limit * page;
-
+    var lim = parseInt(limit)
     if (search && currency && price) {
       const count = await Products.find({
         currency: currency,
@@ -31,7 +30,7 @@ const getProducts = async (req, res) => {
         name: { $regex: search, $options: "i" },
       })
         .sort(price)
-        .limit(12)
+        .limit(lim)
         .skip(skip);
       return res.json({ products, count });
     }
@@ -43,7 +42,7 @@ const getProducts = async (req, res) => {
         name: { $regex: search, $options: "i" },
       })
         .sort(price)
-        .limit(12)
+        .limit(lim)
         .skip(skip);
       return res.json({ products, count });
     }
@@ -56,7 +55,7 @@ const getProducts = async (req, res) => {
         currency: currency,
         name: { $regex: search, $options: "i" },
       })
-        .limit(12)
+        .limit(lim)
         .skip(skip);
       return res.json({ products, count });
     }
@@ -64,7 +63,7 @@ const getProducts = async (req, res) => {
       const count = await Products.find({ currency }).countDocuments();
       const products = await Products.find({ currency })
         .sort(price)
-        .limit(12)
+        .limit(lim)
         .skip(skip);
       return res.json({ products, count });
     }
@@ -72,15 +71,19 @@ const getProducts = async (req, res) => {
       const count = await Products.find({ currency }).countDocuments();
       const products = await Products.find({ currency })
         .sort(createdAt)
-        .limit(12)
+        .limit(lim)
         .skip(skip);
       return res.json({ products, count });
     }
     if (createdAt && search) {
-      const count = await Products.find({ search }).countDocuments();
-      const products = await Products.find({ search })
+      const count = await Products.find({
+        name: { $regex: search, $options: "i" },
+      }).countDocuments();
+      const products = await Products.find({
+        name: { $regex: search, $options: "i" },
+      })
         .sort(createdAt)
-        .limit(12)
+        .limit(lim)
         .skip(skip);
       return res.json({ products, count });
     }
@@ -91,27 +94,33 @@ const getProducts = async (req, res) => {
       const products = await Products.find({
         name: { $regex: search, $options: "i" },
       })
-        .limit(12)
+        .limit(lim)
         .skip(skip);
       return res.json({ products, count });
     }
     if (currency) {
       const count = await Products.find({ currency }).countDocuments();
-      const products = await Products.find({ currency }).limit(12).skip(skip);
+      const products = await Products.find({ currency }).limit(lim).skip(skip);
       return res.json({ products, count });
     }
     if (price) {
       const count = await Products.find({}).countDocuments();
-      const products = await Products.find({}).sort(price).limit(12).skip(skip);
+      const products = await Products.find({}).sort(price).limit(lim).skip(skip);
       return res.json({ products, count });
     }
     if (createdAt) {
       const count = await Products.find({}).countDocuments();
-      const products = await Products.find({}).sort(createdAt).limit(12).skip(skip);
+      const products = await Products.find({})
+        .sort(createdAt)
+        .limit(lim)
+        .skip(skip);
       return res.json({ products, count });
     }
     const count = await Products.find({}).countDocuments();
-    const products = await Products.find({}).sort("-createdAt").limit(12).skip(skip);
+    const products = await Products.find({})
+      .sort("-createdAt")
+      .limit(lim)
+      .skip(skip);
     return res.json({ products, count });
   } catch (error) {
     res.send(error.message);
@@ -120,31 +129,41 @@ const getProducts = async (req, res) => {
 const singleProduct = async (req, res) => {
   try {
     const product = await Products.findById(req.params.id);
-    res.json({product});
+    res.json({ product });
   } catch (error) {
-    res.json({msg:error.message});
+    res.json({ msg: error.message });
   }
 };
-const editProduct=async(req,res)=>{
+const editProduct = async (req, res) => {
   try {
-    const {id}=req.params
-    
-    const product=await Products.findByIdAndUpdate(id,{
-      ...req.body
-    },{new:true})
-    res.json({product})
-  } catch (error) {
-    res.json({msg:error.message})
-  }
-}
-const deleteProduct=async(req,res)=>{
-  try {
-    const {id}=req.params
-    const product=await Products.findByIdAndDelete(id)
-    res.json({msg:`${product.name} deleted Successfully`})
-  } catch (error) {
-    res.json({msg:error.message})
-  }
-}
+    const { id } = req.params;
 
-module.exports = { createProduct, getProducts, singleProduct,editProduct ,deleteProduct};
+    const product = await Products.findByIdAndUpdate(
+      id,
+      {
+        ...req.body,
+      },
+      { new: true }
+    );
+    res.json({ product });
+  } catch (error) {
+    res.json({ msg: error.message });
+  }
+};
+const deleteProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Products.findByIdAndDelete(id);
+    res.json({ msg: `${product.name} deleted Successfully` });
+  } catch (error) {
+    res.json({ msg: error.message });
+  }
+};
+
+module.exports = {
+  createProduct,
+  getProducts,
+  singleProduct,
+  editProduct,
+  deleteProduct,
+};
